b1078d66397c74d2a85cb88e2114e7f35a8a7dc2
Cristin <cristin.baciu@gmail.com>
2018-03-08 17:10:08 +0200

    setup spring boot and vue

---
+++ backend/src/main/java/edit/me/out/backend/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package edit.me.out.backend.repository;
+
+
+import edit.me.out.backend.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByLastName(@Param("lastname") String lastname);
+
+    List<User> findByFirstName(@Param("firstname") String firstname);
+
+}
